79543e2f95b619a02273d4a9352120e33d6fb361
goat <fakecake@icloud.com>
2019-12-30 19:29:27 +0900

    feat: update using Squirrel
    # Conflicts:
    #	XIVLauncher/App.xaml.cs
    #	XIVLauncher/Properties/AssemblyInfo.cs
    #	XIVLauncher/Windows/MainWindow.xaml.cs
    #	XIVLauncher/XIVLauncher.csproj
    #	XIVLauncher/packages.config

--- XIVLauncher/Properties/AssemblyInfo.cs
+++ XIVLauncher/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Windows;
 [assembly: AssemblyTitle("XIVLauncherLite")]
 [assembly: AssemblyDescription("Custom lite launcher for FFXIV.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("goatsoft")]
 [assembly: AssemblyProduct("XIVLauncherLite")]
 [assembly: AssemblyCopyright("Copyright © goaaats 2019")]
 [assembly: AssemblyTrademark("")]
@@ -50,7 +50,9 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.0.0")]
-[assembly: AssemblyFileVersion("4.0.0.0")]
+[assembly: AssemblyVersion("4.5.0.0")]
+[assembly: AssemblyFileVersion("4.5.0.0")]
 [assembly: NeutralResourcesLanguage("en")]
 
+// Squirrel-Awareness
+[assembly: AssemblyMetadata("SquirrelAwareVersion", "1")]
